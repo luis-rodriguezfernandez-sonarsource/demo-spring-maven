c6a1313b1bdc9dcf3607ef7ac61b0bc311ecbc6c
Luis Rodriguez Fernandez <luis.rodriguezfernandez@sonarsource.com>
2025-04-16 16:46:44 +0200

    new code issues

--- src/main/java/com/example/demo/Feature1.java
+++ src/main/java/com/example/demo/Feature1.java
@@ -10,6 +10,13 @@ public class Feature1 {
 
     public void method1() {
         System.out.println("not covered");
-         logger.info("not covered");
+        logger.info("not covered");
+    }
+
+    public void method2() {
+        String bar = null;
+        if (bar.length() > 0) {
+            System.out.println("not covered");
+        }
     }
 }
