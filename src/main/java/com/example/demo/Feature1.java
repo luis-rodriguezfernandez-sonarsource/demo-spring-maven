92348ec360a25935095904d4ef36f0fe86014ca5
Luis Rodriguez Fernandez <luis.rodriguezfernandez@sonarsource.com>
2025-04-16 16:28:33 +0200

    fix code issue by using a logger

--- src/main/java/com/example/demo/Feature1.java
+++ src/main/java/com/example/demo/Feature1.java
@@ -1,9 +1,15 @@
 package com.example.demo;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Feature1 {
+
+    Logger logger = LoggerFactory.getLogger(Feature1.class);
 
     public void method1() {
         System.out.println("not covered");
-        System.out.println("not covered");
+         logger.info("not covered");
     }
 }
