8140f45328e68701d3b86d0ecc48e5d8f768147b
Luis Rodriguez Fernandez <luis.rodriguezfernandez@sonarsource.com>
2025-06-27 15:59:25 +0200

    remove duplications

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,40 +9,4 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-    public void method1() {
-        // Some duplicated code here
-        System.out.println("Hello");
-        System.out.println("World");
-        // more duplicated code
-    }
-
-    public void method2() {
-        // Some duplicated code here
-        System.out.println("Hello");
-        System.out.println("World");
-        // more duplicated code
-    }
-
-    public void method3() {
-        // Some duplicated code here
-        System.out.println("Hello");
-        System.out.println("World");
-        // more duplicated code
-    }
-
-    public void method4() {
-        // Some duplicated code here
-        System.out.println("Hello");
-        System.out.println("World");
-        // more duplicated code
-    }
-
-    public void method5() {
-        // Some duplicated code here
-        System.out.println("Hello");
-        System.out.println("World");
-        // more duplicated code
-    }
-
 }
